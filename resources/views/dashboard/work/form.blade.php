d68b3fbbf3687adc298814faa5923140a101744f
hoxno <mohamed.mouman@gmail.com>
2023-10-21 12:15:48 +0200

    Correction work

--- resources/views/dashboard/work/form.blade.php
+++ resources/views/dashboard/work/form.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <form  class="" action="{{ route($work->exists ? 'dashboard.skill.update' : 'dashboard.skill.store', $work) }}" method="post" enctype="multipart/form-data">
+                    <form  class="" action="{{ route($work->exists ? 'dashboard.work.update' : 'dashboard.work.store', $work) }}" method="post" enctype="multipart/form-data">
                         @csrf
                         @method($work->exists ? 'put' : 'post')
                 
