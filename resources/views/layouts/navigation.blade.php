9e88e91df755c79e760871dd3b0cec80c3e4f983
hoxno <mohamed.mouman@gmail.com>
2023-10-26 18:35:11 +0200

    Navigation de la partie admin

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -73,7 +73,7 @@
                             <x-dropdown-link :href="route('logout')"
                                     onclick="event.preventDefault();
                                                 this.closest('form').submit();">
-                                {{ __('Log Out') }}
+                                {{ __('Se deconnecter') }}
                             </x-dropdown-link>
                         </form>
                     </x-slot>
@@ -93,36 +93,48 @@
     </div>
 
     <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+    <div :class="{'block': open, 'hidden': !open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('dashboard.skill.index')" :active="request()->routeIs('dashboard.skill.index')">
+                {{ __('Mes compétences') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('dashboard.work.index')" :active="request()->routeIs('dashboard.work.index')">
+                {{ __('Mes expériences') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('dashboard.school.index')" :active="request()->routeIs('dashboard.school.index')">
+                {{ __('Mes formations') }}
+            </x-responsive-nav-link>
+            
+            <!-- Vous pouvez ajouter d'autres liens ici -->
         </div>
-
+    
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="px-4">
-                <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
+                <div class="font-medium text-base text-gray-800">{{ Auth::user()->firstname }}</div>
                 <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
             </div>
-
+    
             <div class="mt-3 space-y-1">
                 <x-responsive-nav-link :href="route('profile.edit')">
-                    {{ __('Profile') }}
+                    {{ __('Profil') }}
                 </x-responsive-nav-link>
-
+    
                 <!-- Authentication -->
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
-
+    
                     <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Log Out') }}
+                        onclick="event.preventDefault();
+                        this.closest('form').submit();">
+                        {{ __('Se déconnecter') }}
                     </x-responsive-nav-link>
                 </form>
             </div>
         </div>
     </div>
+    
 </nav>
