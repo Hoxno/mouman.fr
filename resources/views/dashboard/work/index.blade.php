14661eaa95b89b7479282a4146193ecb053e02be
hoxno <mohamed.mouman@gmail.com>
2024-03-02 17:31:28 +0100

    Mise de la partie expérience et formation

--- resources/views/dashboard/work/index.blade.php
+++ resources/views/dashboard/work/index.blade.php
@@ -1,68 +1,44 @@
 <x-app-layout :title="'Mes expériences'">
-    <x-slot name="header">
-        <div class="flex flex-col md:flex-row justify-between items-center">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight md:mt-16">
-                {{ __('Dashboard > Mon CV > Mes expériences') }}
-            </h2>
-            <a href="{{ route('dashboard.work.create') }}" class="mt-6 md:mt-16 py-2.5 px-7 border-2 cursor-pointer hover:bg-[#111827] hover:text-[#fff]">Ajouter</a>
-        </div>
-    </x-slot>
-
+    @section('title', 'Mes expériences')
     <div class="py-12">
-        <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <x-flash></x-flash>
+            <div class="bg-white border border-gray-200 shadow-md shadow-black/3 p-6 rounded-md dark:bg-dark__body">
+                <div class="flex justify-between mb-4 items-start">
+                    <div class="font-medium">
+                        <x-primary-button>
+                            <a href="{{ route('dashboard.work.create') }}">Ajouter une expérience</a>
+                        </x-primary-button>
+                    </div>
+                </div>
                 <div class="p-6 text-gray-900">
-                    <div class="overflow-x-auto max-h-96">
+                    <div class="overflow-hidden">
                         <div class="max-h-96 overflow-y-auto">
-                            <table class="table-auto w-full">
-                                <thead>
+                            <table class="w-full border-collapse bg-white text-left text-sm text-gray-500 dark:bg-dark__body">
+                                <thead class="bg-gray-50 dark:bg-dark__body">
                                     <tr>
-                                        <th class="p-3.5 border-b-2 text-center border-r-[1px]">Titre</th>
-                                        <th class="p-3.5 border-b-2 text-center border-r-[1px]">Entreprise</th>
-                                        <th class="p-3.5 border-b-2 text-center border-r-[1px]">Description</th>
-                                        <th class="p-3.5 border-b-2 text-center">Actions</th>
+                                        <th class="text-[12px] uppercase tracking-wide font-medium text-gray-600 py-6 px-4  text-left rounded-tl-md rounded-bl-md"></th>
+                                        <th class="text-[12px] uppercase tracking-wide font-medium text-gray-600 py-6 px-4  text-left rounded-tl-md rounded-bl-md">Titre</th>
+                                        <th class="text-[12px] uppercase tracking-wide font-medium text-gray-600 py-6 px-4  text-left rounded-tl-md rounded-bl-md">Employeur</th>
+                                        <th class="text-[12px] uppercase tracking-wide font-medium text-gray-600 py-6 px-4  text-left rounded-tl-md rounded-bl-md">Date de début</th>
+                                        <th class="text-[12px] uppercase tracking-wide font-medium text-gray-600 py-6 px-4  text-left rounded-tl-md rounded-bl-md">Date de fin</th>
+                                        <th class="text-[12px] uppercase tracking-wide font-medium text-gray-600 py-6 px-4  text-left rounded-tl-md rounded-bl-md"></th>
                                     </tr>
                                 </thead>
-                                <tbody>
+                                <tbody class="divide-y divide-gray-100 border-t border-gray-100">
                                     @foreach ($works as $work)
-                                        <tr>
+                                        <tr class="hover:bg-gray-50">
+                                            <td class="flex gap-3 px-6 py-4 font-normal text-gray-900">
+                                                <input type="checkbox" x-model="selected" value="{{ $work->id }}">
+                                            </td>
                                             <td class="p-3.5 border-b-[1px] border-r-[1px]">{{ $work->title }}</td>
-                                            <td class="p-3.5 border-b-[1px] border-r-[1px]">{{ $work->company }}</td>
-                                            <td class="p-3.5 border-b-[1px] border-r-[1px]">{!! nl2br($work->description) !!}</td>
+                                            <td class="p-3.5 border-b-[1px] border-r-[1px]">{{ $work->employer }}</td>
+                                            <td class="p-3.5 border-b-[1px] border-r-[1px]">{{ $work->start_date }}</td>
+                                            <td class="p-3.5 border-b-[1px] border-r-[1px]">{{ $work->end_date }}</td>
                                             <td class="p-3.5 border-b-[1px]">
                                                 <div class="flex gap-2 justify-end">
-                                                    <x-primary-button>
-                                                        <a href="{{ route('dashboard.work.edit', $work) }}">Editer</a>
-                                                    </x-primary-button>
-                                                    <x-danger-button
-                                                        x-data=""
-                                                        x-on:click.prevent="$dispatch('open-modal', 'confirm-deletion')"
-                                                    >{{ __('Suppression') }}</x-danger-button>
-
-                                                    <x-modal name="confirm-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
-                                                        <form method="post" action="{{ route('dashboard.work.destroy', $work) }}" class="p-6">
-                                                        @csrf
-                                                        @method('delete')
-
-                                                            <h2 class="text-lg font-medium text-gray-900">
-                                                                {{ __('Confirmer la suppression') }}
-                                                            </h2>
-
-                                                            <p class="mt-1 text-sm text-gray-600">
-                                                                {{ __('Êtes-vous sûr de vouloir supprimer cette expérience ?') }}
-                                                            </p>
-
-                                                            <div class="mt-6 flex justify-end">
-                                                                <x-secondary-button x-on:click="$dispatch('close')">
-                                                                    {{ __('Annuler') }}
-                                                                </x-secondary-button>
-
-                                                                <x-danger-button class="ml-3">
-                                                                    {{ __('Supprimer') }}
-                                                                </x-danger-button>
-                                                            </div>
-                                                        </form>
-                                                    </x-modal>
+                                                    @include('dashboard.work.partials.edit')
+                                                    @include('dashboard.work.partials.delete')
                                                 </div>
                                             </td>
                                         </tr>
